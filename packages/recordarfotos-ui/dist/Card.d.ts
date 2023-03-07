18cb7064a3c73d709073e12454c133eddd4a29da
Alex da Silva <alexkads@gmail.com>
2023-03-06 21:06:08 -0300

    Versão 0.0.1

---
+++ packages/recordarfotos-ui/dist/Card.d.ts
@@ -0,0 +1,7 @@
+declare const Card: ({ title, cta, href, }: {
+    title: string;
+    cta: string;
+    href: string;
+}) => JSX.Element;
+
+export { Card };
---
+++ packages/recordarfotos-ui/dist/index.d.ts
@@ -0,0 +1,2 @@
+export { Button } from './Button.js';
+export { Card } from './Card.js';
